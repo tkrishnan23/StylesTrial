9038fef08e5e92ea24717463ea1d94d3d1985b42
tkrishnan23 <90794124+tkrishnan23@users.noreply.github.com>
2022-09-15 08:18:42 -0500

    added layout/cleaned up design

--- app/src/main/java/com/example/stylestrialproject/MainActivity.java
+++ app/src/main/java/com/example/stylestrialproject/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.stylestrialproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchScreens(View view){
+        Intent intent = new Intent(this, SwipeActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.drawable.slide_in_left, R.drawable.slide_out_right);
     }
 }
